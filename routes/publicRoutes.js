a3b472a9fcb2b35f8f3a5dbfef86611c26deb92a
Ale <macbooksale@MacBook-Air-Ale.local>
2026-05-13 02:52:51 +0700

    feat: add KupsController and KUPS endpoints to public routes

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const PublicController = require('../controllers/PublicController');
+const KupsController = require('../controllers/KupsController');
 
 // 1. Search & List (Card UI)
 router.get('/search', PublicController.search);
@@ -14,7 +15,11 @@ router.get('/stats', PublicController.getStats);
 // 4. Detail Permit
 router.get('/permit/:id', PublicController.getDetail);
 
-// Legal Dashboard Endpoint
+// 5. Legal Dashboard Endpoint
 router.get('/dashboard/legal', PublicController.getLegalDashboardStats);
+
+// 6. KUPS Routes
+router.get('/kups', KupsController.search);
+router.get('/kups/filters', KupsController.getFilters);
 
 module.exports = router;
